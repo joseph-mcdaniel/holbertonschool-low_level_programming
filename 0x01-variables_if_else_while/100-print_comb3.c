7d83f60f11ace75cffca975d7e0514dc3c862cf7
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-12 01:03:20 +0000

    Print number combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 /**
- * main - print all combinations
- *
- * Description: while, for, if
- * Return: returns 0
+ *main -  prints all possible different combinations of two digits
+ *Return: 0
  */
 int main(void)
 {
-  int numone = 0, numtwo, i = 1;
+int numone = 0, numtwo, i = 1;
 
-  while (i <= 9)
+ while (i <= 9)
   {
-          for (numtwo = i; numtwo <= 9; numtwo++)
+    for (numtwo = i; numtwo <= 9; numtwo++)
+      {
+	putchar('0' + numone);
+	putchar('0' + numtwo);
+	if (numone < 9 && i != 9)
 	  {
-	         putchar('0' + numone);
-	         putchar('0' + numtwo);
-		 if (numone < 9 && i != 9)
-		 {
-		       putchar(',');
-		       putchar(' ');
-		 }
+	    putchar(',');
+	    putchar(' ');
 	  }
-	  i++;
-	  numone++;
+      }
+    i++;
+    numone++;
   }
-  putchar('\n');
-  return (0);
+ putchar('\n');
+ return (0);
 }
