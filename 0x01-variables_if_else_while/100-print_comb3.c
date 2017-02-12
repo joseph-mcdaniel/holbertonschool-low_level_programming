057ab03aa20530b65d0170e46e917f842623cb33
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-12 00:57:17 +0000

    Print number combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,20 +10,20 @@ int main(void)
   int numone = 0, numtwo, i = 1;
 
   while (i <= 9)
-    {
-      for (numtwo = i; numtwo <= 9; numtwo++)
-	{
-	  putchar('0' + numone);
-	  putchar('0' + numtwo);
-	  if (numone < 9 && i != 9)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-      i++;
-      numone;
-    }
+  {
+          for (numtwo = i; numtwo <= 9; numtwo++)
+	  {
+	         putchar('0' + numone);
+	         putchar('0' + numtwo);
+		 if (numone < 9 && i != 9)
+		 {
+		       putchar(',');
+		       putchar(' ');
+		 }
+	  }
+	  i++;
+	  numone++;
+  }
   putchar('\n');
   return (0);
 }
