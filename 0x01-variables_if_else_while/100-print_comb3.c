df17d412e0fbafca800b64286720724c8697b26f
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-12 00:51:48 +0000

    Print number combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,32 +2,28 @@
 /**
  * main - print all combinations
  *
- * Description: Use for loops
+ * Description: while, for, if
  * Return: returns 0
  */
-
 int main(void)
 {
-  int i, j, n;
+  int numone = 0, numtwo, i = 1;
 
-  i = 0;
-  n = 1;
-
-  while (n <= 9)
-  {
-    for (j = n; j <= 9; j++)
-      {
-	putchar('0' + i);
-	putchar('0' + j);
-	if (n < 9 && i != 9)
-	  {
-	    putchar(',');
-	    putchar(' ');
-	  }
-      }
-    n++;
-    i++;
-  }
+  while (i <= 9)
+    {
+      for (numtwo = i; numtwo <= 9; numtwo++)
+	{
+	  putchar('0' + numone);
+	  putchar('0' + numtwo);
+	  if (numone < 9 && i != 9)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+      i++;
+      numone;
+    }
   putchar('\n');
   return (0);
 }
