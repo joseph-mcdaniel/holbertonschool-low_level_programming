b1bc5c2d0f7e8a87a203596dadb14ac29906ca4a
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-13 02:27:44 +0000

    Print combination of two different digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - etc, etc
+ *
+ * Description: Print combination only once
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i < 9)
+	{
+		for (j = i; j <= 9; j++)
+		{
+			if (i != j && i < j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		i++;
+	}
+	putchar ('\n');
+	return (0);
+}
