0cc5866cd487fea60c4390c809205eed3a3dc060
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-14 23:27:28 +0000

    Print 'Holberton'

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
 
 /**
- *
  * main - print Holberton
  *
+ * Description: use _putchar
  * Return: returns 0
  */
 
-int main(void) 
+int main(void)
 {
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar('\n');
-  return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
 }
