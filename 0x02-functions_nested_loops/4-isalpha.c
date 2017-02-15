d6e8587ad2b15f8b50e37ebc95c539402a207608
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-15 02:19:29 +0000

    Check for upper/lower alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks if upper
+ *
+ * @c: upper or lowercase letters
+ * Description: Use conditional statements
+ * Return: 1 if upper/lower letter,
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
