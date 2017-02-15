0df83ae46f734a8149858525120c86ca18f8c738
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-15 07:54:11 +0000

    Add two integers, return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * add - return result of a+b
+ * @a: first input value
+ * @b: second input value
+ *
+ * Description: Use three variables
+ * Return: return c
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
