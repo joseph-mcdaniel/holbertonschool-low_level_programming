108fe9b153f3941ae0be8d52487e59abbee93f6a
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-15 20:40:48 +0000

    Print natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,23 +12,22 @@
 void print_to_98(int n)
 {
 	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	while (n <= 98)
+	{
+		if (n != 98)
 		{
 			printf("%d, ", n);
-			n--;
 		}
-	while (n <= 98)
+
+		else if (n == 98)
 		{
-			if (n != 98)
-			{
-				printf("%d, ", n);
-			}
-
-			else if (n == 98)
-			{
-				printf("%d\n", n);
-			}
-
-			n++;
+			printf("%d\n", n);
 		}
+		n++;
+	}
 
 }
