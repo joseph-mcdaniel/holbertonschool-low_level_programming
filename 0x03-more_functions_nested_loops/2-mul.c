ce6711e20bab3df64cd121544b1cc9faa908e7a5
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-16 23:00:35 +0000

    Multiply two integers

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - check the code for Holberton School students.
+ * @a: first input value
+ * @b: second input value
+ *
+ * Return: return c.
+ */
+int mul(int a, int b)
+{
+
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
