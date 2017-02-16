ec886356dade3aea2f0614df3a643633dd3ba7cd
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-16 21:34:08 +0000

    Checks for uppercase letters

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isupper - check for uppercase
+ * @c: input value
+ *
+ * Return: 1 if upper, else 0
+ */
+int _isupper(int c)
+{
+	if (c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+			}
+}
