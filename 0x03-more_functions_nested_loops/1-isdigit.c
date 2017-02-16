3fc0da5e1757eeafea7da7cc572a4a616cc364b0
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-16 22:18:41 +0000

    Checks for digit, returns 1

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: input value
+ *
+ * Return - return 1 if digit, else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
