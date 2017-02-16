9d496ac8d56c2d05de4a75f65b03f1eeea10d312
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-16 22:47:39 +0000

    Add description for return value

--- 0x03-more_functions_nested_loops/1-isdigit.c
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 /**
  * _isdigit - checks for a digit
- * @c: input value
+ * @c: input value of _isdigit
  *
+ *
+ * Description: conditional to specify range of digits
  * Return - return 1 if digit, else 0
+ * @1: value of digit
+ * @0: value of anything else
  */
 int _isdigit(int c)
 {
