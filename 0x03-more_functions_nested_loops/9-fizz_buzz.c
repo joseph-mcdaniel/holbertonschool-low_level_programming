1c8e7663b72cf6506b072cbc76349ad424f761e1
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-17 08:48:41 +0000

    Print Fizz for multiples of 3, Buzz for multiples of 5, FizzBuzz for both, else print number

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - for multiples of 3 print fizz
+ * for multiples of 5 print buzz
+ * for multiples of both print fizzbuzz
+ * else print number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", i);
+				}
+	printf("\n");
+	return (0);
+}
