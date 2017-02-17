9ea924bcc96913809824029594b343f68d779d39
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-17 02:50:20 +0000

    Repeat char based on n

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,10 @@
 #include "holberton.h"
 /**
+ * print_line - repeat char
+ * @n: input value to tell how
+ * many times char repeats
  *
- *
- *
+ * Return - 0
  */
 void print_line(int n)
 {
@@ -10,7 +12,7 @@ void print_line(int n)
 
 	for (j = 0; j < n; j++)
 	{
-		_putchar('_' );
+		_putchar('_');
 	}
 	_putchar('\n');
 }
