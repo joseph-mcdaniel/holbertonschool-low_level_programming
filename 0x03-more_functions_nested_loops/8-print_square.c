36f8e00825404ac5c3422ff062ac161207460458
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-17 07:47:24 +0000

    Print char to form square based on size

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - print char
+ * to make a square
+ * @size: input value
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
