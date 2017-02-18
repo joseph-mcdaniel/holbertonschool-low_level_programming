364af5499fecc6116e3aaa9a8aca46fcd72e1218
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-18 08:53:13 +0000

    Print integers only using putchar

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,16 +1,25 @@
 #include "holberton.h"
 /**
+ * print_number - print int using putchar
+ * @n: input value
  *
- *
+ * Return: 0
  */
 
+
+void print_unumber(unsigned int n)
+{
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+
 void print_number(int n)
-{	if (n < 0)
+{
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+	print_unumber((unsigned int) n);
 }
