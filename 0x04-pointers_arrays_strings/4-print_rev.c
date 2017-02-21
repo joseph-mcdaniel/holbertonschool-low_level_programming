1035e5a6d11cf482eaebe5fad8f2dbe6b7baabfa
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-21 22:55:45 +0000

    Print string in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_rev - print string in reverse
+ * @s: input value
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
