e258192754ad3ccb04439a82d4c678ed1c041227
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-23 07:41:30 +0000

    Print half of string

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,48 @@
 void puts_half(char *str)
 {
 	int c;
+	int length;
 
-	for (c = 0; str[c] != '\0'; c++)
+	length = _strlen(str);
+
+	if (str == '\0')
 	{
-		if (c >= 5)
+		return;
+	}
+
+	if (length % 2 != 0)
+	{
+		for (c = ((length - 1) / 2) + 1; c < length; c++)
+		{
+			_putchar(str[c]);
+		}
+	}
+	else
+	{
+		for (c = length / 2; c < length; c++)
 		{
 			_putchar(str[c]);
 		}
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - return length of
+ * string
+ * @s: input value
+ *
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 }
