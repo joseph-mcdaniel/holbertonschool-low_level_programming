08bd5839e804df234b7e870fb7a760feb7ef4813
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-24 00:14:46 +0000

    Concatenate two strings

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- *
- *
- *
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
