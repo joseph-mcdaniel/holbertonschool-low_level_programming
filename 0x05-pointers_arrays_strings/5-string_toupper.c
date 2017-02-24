f33f4d152171d832cb2724bbcc3f6188083774d0
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-24 05:14:11 +0000

    Change all letters of a string lowercase to uppercase

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * stromg_toupper - change all lowercase to uppercase
+ * @n: pointer
+ * @i: input value
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] > 'a' && n[i] < 'z')
+			n[i] = n[i]-32;
+		i++;
+	}
+	return (n);
+
+}
