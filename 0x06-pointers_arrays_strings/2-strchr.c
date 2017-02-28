d14aab54776c252a0b5adb78f3c904553befd2cf
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-28 03:29:08 +0000

    Locates a character in a string.

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr - returns a pointer to the first
+ * occurence of the character c in the string
+ * @s: string
+ * @c: character
+ *
+ * Return: s + i
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+	}
+	return (s + i);
+}
