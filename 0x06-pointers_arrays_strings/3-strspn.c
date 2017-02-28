cc32069fa0c71ca1eed517fe050b2fcff0fc53dd
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-02-28 05:15:28 +0000

    Get the length of a prefix substring

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+		{
+			break;
+		}
+	}
+	return (j);
+}
