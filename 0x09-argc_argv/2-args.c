4a337bd2886451088aee9fee0a9baea3d3fe4511
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-04 23:32:49 +0000

    Print all arguments received

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - print all arg received
+ * @argc: arg
+ * @argv: arg
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
