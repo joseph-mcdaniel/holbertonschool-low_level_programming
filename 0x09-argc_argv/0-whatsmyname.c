d76eda2ceb71b47a3bc976619aefdf80a50356f4
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-05 00:13:58 +0000

    Print program name

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  *
  * Return: 0
  */
-char main(int argv __attribute__((unused)), char *argv[])
+char main(int __attribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
