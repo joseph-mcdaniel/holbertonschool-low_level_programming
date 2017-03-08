ad5021a080b84d2f4f841ecfe063d2d9afe0bbdc
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-08 07:06:43 +0000

    Concatenate two strings

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -16,6 +16,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = "";
+		return (NULL);
 	}
 	else
 		string1 = _strlen(s1);
@@ -23,6 +24,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 	{
 		s2 = "";
+		return (NULL);
 	}
 	else
 		string2 = _strlen(s2);
