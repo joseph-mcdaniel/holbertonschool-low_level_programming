b3b6ec1cc42ed20f396edde26766ddca9f8794fb
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-10 03:45:09 +0000

    Update prototypes

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -5,3 +5,6 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
