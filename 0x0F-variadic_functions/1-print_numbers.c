f1db3f787967f0b320f70c699e3728aefc1237ef
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-18 21:14:47 +0000

    Fix condition to stop string print

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -13,14 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(arguments, n);
 
-	if (separator == NULL)
-		return;
-
 	for (i = 0; i < n; i++)
 	{
 		num = va_arg(arguments, unsigned int);
 
-		if (i == n - 1)
+		if (i == n - 1 || separator == NULL)
 			printf("%i", num);
 		else
 			printf("%i%s", num, separator);
