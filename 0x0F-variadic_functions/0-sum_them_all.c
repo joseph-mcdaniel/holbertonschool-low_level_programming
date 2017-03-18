f3e1cb102c23e831927129db680f996857f160b2
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-18 21:04:37 +0000

    Move NULL condition out of loop

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -12,10 +12,11 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(arguments, n);
 
+	if (n == 0)
+		return (0);
+
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-			return (0);
 		sum += va_arg(arguments, unsigned int);
 	}
 	va_end(arguments);
