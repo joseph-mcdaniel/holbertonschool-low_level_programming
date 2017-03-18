ac7d27fdb6ffbb580dfac82e318557de83d0dc70
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-18 21:35:30 +0000

    Print any type via variadic func and switch case statement

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+/**
+ * print_all - print any data type
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i;
+
+
+	va_start(args, format);
+
+	i = 0;
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			printf("%s", va_arg(args, char *));
+			break;
+
+			printf("(nil)");
+			break;
+		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+		     format[i] == 's') && format[(i + 1)] != '\0')
+			printf(", ");
+
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
