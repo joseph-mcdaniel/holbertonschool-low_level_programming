ce0b599f3187ae783c515812e2f38a24f311b6b7
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-24 04:25:09 +0000

    Include more libraries and prototypes

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
-
+#include <string.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -23,4 +24,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 #endif
