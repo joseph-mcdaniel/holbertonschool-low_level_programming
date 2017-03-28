b4e943b0f9c781a410ca2ba4627d8a70ab22d421
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-28 20:06:53 +0000

    Free linked list

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free a linked list
+ * @head: pointer to the address of a pointer to head
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tempnode;
+
+	while (*head != NULL)
+	{
+		tempnode = *head;
+		*head = (*head)->next;
+		free(tempnode);
+	}
+}
