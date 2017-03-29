3889e3fb7bebe028941e908072aaf9855c78d6eb
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-29 03:46:52 +0000

    Edit assignment of newnode->next

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count;
 
 	newnode->n = n;
-	newnode->next = 0;
+	newnode->next = NULL;
 
 	if (temp == NULL && idx == 0)
 	{
