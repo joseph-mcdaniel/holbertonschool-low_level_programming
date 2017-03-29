5ad0e8eb16dbeda7434292a42fd41bf950bc0811
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-29 03:43:28 +0000

    Edit final conditional to check if newnode is null

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 		}
 	}
-	if (newnode == NULL)
+	if (newnode == NULL && temp == NULL)
 		return (NULL);
 	return (newnode);
 }
