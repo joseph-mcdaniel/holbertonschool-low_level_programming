1774daba2d90a37a625171a77c30718eeba0019f
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-31 05:26:07 +0000

    Rearrange edge case conditionals outside of loop

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -11,14 +11,16 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int unum;
 
+	if (b == NULL)
+		return (0);
+
+	if (*b != '1' && *b != '0')
+		return (0);
+
 	unum = 0;
 	while (*b)
 	{
-		if (b == NULL)
-			return (0);
-		else if (*b != '1' && *b != '0')
-			return (0);
-		else if (*b == '1')
+		if (*b == '1')
 			unum = unum << 1 | 1;
 		else if (*b == '0')
 			unum <<= 1;
