fc8b541cfd951670cae0597c0aa816f826d11bd9
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-04-03 01:38:32 +0000

    Checks for the endianness, add prototype

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int x = 1;
+
+	char *n = (char*)&x;
+
+	return (*n);
+}
