662232fa9845919416380e2fa9db2e66462327f6
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-04-07 03:36:41 +0000

    Edit error function calls

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -66,6 +66,6 @@ int main(int argc, char *argv[])
 			w_error(argv[2]);
 		rd_val = read(r_fd, buffer, BUF_SIZE);
 	}
-	return (close(r_fd) == -1 ? c_error(arg[1]) : close(w_fd) == -1 ?
-		c_error(arg[2]) : 0);
+	return (close(r_fd) == -1 ? c_error(r_fd) : close(w_fd) == -1 ?
+		c_error(w_fd) : 0);
 }
