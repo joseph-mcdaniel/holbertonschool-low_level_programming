ca02681e89a377ca5912267b2769602c73e23c60
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-07-28 05:07:49 +0000

    add null conditionals

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -11,9 +11,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (!new_node)
+	if (parent == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
