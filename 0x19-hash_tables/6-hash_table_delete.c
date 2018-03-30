0e2f5ecdb241da10cc56dd3817dcf1472f7e55d6
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-03-30 11:10:15 +0000

    delete hash table

---
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		for (temp = ht->array[i]; temp != NULL; temp = temp->next)
+		{
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
