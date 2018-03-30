c732e0e4f01a04133ab6935b575f21b905291961
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-03-30 08:31:06 +0000

    check if hash table / key exists

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int idx;
 	hash_node_t *temp;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	/* get index of key */
 	idx = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[idx];
