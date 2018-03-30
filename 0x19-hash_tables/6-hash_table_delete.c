a3c766284b80afcbdb1a35fcfee97b488355a7ba
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-03-30 11:30:49 +0000

    store values from temp before free

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -8,18 +8,20 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *temp;
+	hash_node_t *temp, *temp2;
 
 	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		for (temp = ht->array[i]; temp != NULL; temp = temp->next)
+		for (temp = ht->array[i]; temp != NULL; )
 		{
+			temp2 = temp->next;
 			free(temp->key);
 			free(temp->value);
 			free(temp);
+			temp = temp2;
 		}
 	}
 	free(ht->array);
